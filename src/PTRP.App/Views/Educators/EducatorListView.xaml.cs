160a4cc600b90624d2f6ad9229b4506bb55dfc95
Marco Cavallo <cavallo.marco@gmail.com>
2026-01-31 11:42:24 +0100

    feat: add EducatorListView code-behind

---
+++ src/PTRP.App/Views/Educators/EducatorListView.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+
+namespace PTRP.App.Views.Educators;
+
+/// <summary>
+/// Interaction logic for EducatorListView.xaml
+/// </summary>
+public partial class EducatorListView : UserControl
+{
+    public EducatorListView()
+    {
+        InitializeComponent();
+    }
+}
